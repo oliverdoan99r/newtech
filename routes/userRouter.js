9db9459d922e9be3613501aae4c3f5007fa8ab7d
Mai Thư <thu261999@gmail.com>
2020-12-17 10:16:31 +0700

    login user

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,4 +4,6 @@ const userCtrl = require('../controllers/userCtrl')
 router.post('/register', userCtrl.register)
 
 router.post('/activation', userCtrl.activateEmail)
+
+router.post('/login', userCtrl.login)
 module.exports = router
